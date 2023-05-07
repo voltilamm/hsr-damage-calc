3a2c11ff42aa4e653394be0c07b565cc332585d4
voltilamm <voltilamm@icloud.com>
2023-05-07 15:44:08 +0900

    feat: add base damage calc

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,81 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { useMemo } from "react";
+import { useForm } from "react-hook-form";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Grid,
+  InputAdornment,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+type FormValues = {
+  scalingAttribute: number;
+  skillMultiplier: number;
+};
 
 function App() {
+  const { register, watch } = useForm<FormValues>({
+    defaultValues: {
+      scalingAttribute: 2000,
+      skillMultiplier: 100,
+    },
+  });
+
+  const [scalingAttribute, skillMultiplier] = watch([
+    "scalingAttribute",
+    "skillMultiplier",
+  ]);
+
+  const baseDamage = useMemo(
+    () => (scalingAttribute * skillMultiplier) / 100,
+    [scalingAttribute, skillMultiplier]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <AppBar component="nav" position="sticky">
+        <CssBaseline />
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            ダメージ計算機
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container component="main" sx={{ p: 2 }}>
+        <Grid item xs={12} sm={12} md={6}>
+          <TextField
+            {...register("scalingAttribute")}
+            id="scaling-attribute"
+            label="攻撃力"
+            type="number"
+            InputLabelProps={{ shrink: true }}
+            variant="standard"
+          />
+          <TextField
+            {...register("skillMultiplier")}
+            id="skill-multiplier"
+            label="倍率"
+            type="number"
+            InputLabelProps={{ shrink: true }}
+            InputProps={{
+              endAdornment: <InputAdornment position="end">%</InputAdornment>,
+            }}
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+          <Typography variant="h6" component="div">
+            計算結果
+          </Typography>
+          <Typography variant="h6" component="div">
+            {baseDamage}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
