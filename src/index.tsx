e34e952f992bbbce006420252f033b212cbeb030
voltilamm <voltilamm@icloud.com>
2023-05-07 22:10:05 +0900

    feat: set theme to match system preference

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createTheme, ThemeProvider } from "@mui/material";
-
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
